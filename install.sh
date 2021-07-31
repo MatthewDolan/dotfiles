99ac4897444ebecba69e0a83fbe3976ec119ae09
Matthew Dolan <MatthewDolan@users.noreply.github.com>
2021-07-31 17:34:02 -0500

    add install oh-my-zsh

--- install.sh
+++ install.sh
@@ -3,8 +3,17 @@ set -e
 
 echo "Setting up your Computer..."
 
-echo "Creating a Development folder..."
-mkdir -p "$HOME/Development"
+# create development directory
+if ! [ -d "$HOME/Development" ]; then
+  echo "Creating a Development folder..."
+  mkdir -p "$HOME/Development"
+fi
+
+# install oh-my-zsh
+if ! [ -d "$HOME/.oh-my-zsh" ]; then
+  echo "Installing oh-my-zsh..."
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --keep-zshrc
+fi
 
 # symlink dotfiles
 echo "Symlinking dotfiles..."
