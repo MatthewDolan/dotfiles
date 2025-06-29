01308bad2dccfdddc8d8787aa16d3df398eadbd6
Matthew Dolan <MatthewDolan@users.noreply.github.com>
2025-06-28 21:05:01 -0700

    Remove bats check from test script

--- ci/test.sh
+++ ci/test.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if ! command -v bats >/dev/null 2>&1; then
-  echo "bats not found. Please install bats to run tests." >&2
-  exit 1
-fi
-
 bats -r tests "$@"
